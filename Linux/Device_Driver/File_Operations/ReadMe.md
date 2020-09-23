c775ba9ac42753a074b4b41d72a6d4860ec269ac
EmbeTronicX <embetronicx@gmail.com>
2020-09-23 07:53:27 +0530

    added file operation example source code

---
+++ Linux/Device_Driver/File_Operations/ReadMe.md
@@ -0,0 +1,4 @@
+This is just a basic linux device driver which explains about the file operations.
+
+Please refer this URL for the complete tutorial of this example source code.
+https://embetronicx.com/tutorials/linux/device-drivers/cdev-structure-and-file-operations-of-character-drivers/
---
+++ Linux/Device_Driver/File_Operations/driver.c
@@ -0,0 +1,137 @@
+/***************************************************************************//**
+*  \file       driver.c
+*
+*  \details    Simple Linux device driver (File Operations)
+*
+*  \author     EmbeTronicX
+*
+* *******************************************************************************/
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kdev_t.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+
+dev_t dev = 0;
+static struct class *dev_class;
+static struct cdev etx_cdev;
+
+/*
+** Function Prototypes
+*/
+static int      __init etx_driver_init(void);
+static void     __exit etx_driver_exit(void);
+static int      etx_open(struct inode *inode, struct file *file);
+static int      etx_release(struct inode *inode, struct file *file);
+static ssize_t  etx_read(struct file *filp, char __user *buf, size_t len,loff_t * off);
+static ssize_t  etx_write(struct file *filp, const char *buf, size_t len, loff_t * off);
+
+static struct file_operations fops =
+{
+    .owner      = THIS_MODULE,
+    .read       = etx_read,
+    .write      = etx_write,
+    .open       = etx_open,
+    .release    = etx_release,
+};
+
+/*
+** This fuction will be called when we open the Device file
+*/
+static int etx_open(struct inode *inode, struct file *file)
+{
+        printk(KERN_INFO "Driver Open Function Called...!!!\n");
+        return 0;
+}
+
+/*
+** This fuction will be called when we close the Device file
+*/
+static int etx_release(struct inode *inode, struct file *file)
+{
+        printk(KERN_INFO "Driver Release Function Called...!!!\n");
+        return 0;
+}
+
+/*
+** This fuction will be called when we read the Device file
+*/
+static ssize_t etx_read(struct file *filp, char __user *buf, size_t len, loff_t *off)
+{
+        printk(KERN_INFO "Driver Read Function Called...!!!\n");
+        return 0;
+}
+
+/*
+** This fuction will be called when we write the Device file
+*/
+static ssize_t etx_write(struct file *filp, const char __user *buf, size_t len, loff_t *off)
+{
+        printk(KERN_INFO "Driver Write Function Called...!!!\n");
+        return len;
+}
+
+/*
+** Module Init function
+*/
+static int __init etx_driver_init(void)
+{
+        /*Allocating Major number*/
+        if((alloc_chrdev_region(&dev, 0, 1, "etx_Dev")) <0){
+                printk(KERN_INFO "Cannot allocate major number\n");
+                return -1;
+        }
+        printk(KERN_INFO "Major = %d Minor = %d \n",MAJOR(dev), MINOR(dev));
+
+        /*Creating cdev structure*/
+        cdev_init(&etx_cdev,&fops);
+
+        /*Adding character device to the system*/
+        if((cdev_add(&etx_cdev,dev,1)) < 0){
+            printk(KERN_INFO "Cannot add the device to the system\n");
+            goto r_class;
+        }
+
+        /*Creating struct class*/
+        if((dev_class = class_create(THIS_MODULE,"etx_class")) == NULL){
+            printk(KERN_INFO "Cannot create the struct class\n");
+            goto r_class;
+        }
+
+        /*Creating device*/
+        if((device_create(dev_class,NULL,dev,NULL,"etx_device")) == NULL){
+            printk(KERN_INFO "Cannot create the Device 1\n");
+            goto r_device;
+        }
+        printk(KERN_INFO "Device Driver Insert...Done!!!\n");
+	    return 0;
+
+r_device:
+        class_destroy(dev_class);
+r_class:
+        unregister_chrdev_region(dev,1);
+        return -1;
+}
+
+/*
+** Module exit function
+*/
+static void __exit etx_driver_exit(void)
+{
+        device_destroy(dev_class,dev);
+        class_destroy(dev_class);
+        cdev_del(&etx_cdev);
+        unregister_chrdev_region(dev, 1);
+	    printk(KERN_INFO "Device Driver Remove...Done!!!\n");
+}
+
+module_init(etx_driver_init);
+module_exit(etx_driver_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("EmbeTronicX <embetronicx@gmail.com>");
+MODULE_DESCRIPTION("Simple Linux device driver (File Operations)");
+MODULE_VERSION("1.3");
+
