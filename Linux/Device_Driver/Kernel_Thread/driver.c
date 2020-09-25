e9bce0dc720ebe60f0ff1357a409906ee7db02ce
EmbeTronicX <embetronicx@gmail.com>
2020-09-25 14:04:27 +0530

    added kernel thread example

---
+++ Linux/Device_Driver/Kernel_Thread/driver.c
@@ -0,0 +1,191 @@
+/***************************************************************************//**
+*  \file       driver.c
+*
+*  \details    Simple Linux device driver (Kernel Thread)
+*
+*  \author     EmbeTronicX
+*
+* *******************************************************************************/
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kdev_t.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include<linux/slab.h>                 //kmalloc()
+#include<linux/uaccess.h>              //copy_to/from_user()
+#include <linux/kthread.h>             //kernel threads
+#include <linux/sched.h>               //task_struct 
+#include <linux/delay.h>
+ 
+ 
+dev_t dev = 0;
+static struct class *dev_class;
+static struct cdev etx_cdev;
+ 
+static int __init etx_driver_init(void);
+static void __exit etx_driver_exit(void);
+ 
+static struct task_struct *etx_thread;
+ 
+/*
+** Function Prototypes
+*/
+/*************** Driver Fuctions **********************/
+static int etx_open(struct inode *inode, struct file *file);
+static int etx_release(struct inode *inode, struct file *file);
+static ssize_t etx_read(struct file *filp, 
+                char __user *buf, size_t len,loff_t * off);
+static ssize_t etx_write(struct file *filp, 
+                const char *buf, size_t len, loff_t * off);
+ /******************************************************/
+ 
+int thread_function(void *pv);
+
+/*
+** Thread
+*/
+int thread_function(void *pv)
+{
+    int i=0;
+    while(!kthread_should_stop()) {
+        printk(KERN_INFO "In EmbeTronicX Thread Function %d\n", i++);
+        msleep(1000);
+    }
+    return 0;
+}
+
+/*
+** File operation sturcture
+*/ 
+static struct file_operations fops =
+{
+        .owner          = THIS_MODULE,
+        .read           = etx_read,
+        .write          = etx_write,
+        .open           = etx_open,
+        .release        = etx_release,
+};
+
+/*
+** This fuction will be called when we open the Device file
+*/  
+static int etx_open(struct inode *inode, struct file *file)
+{
+        printk(KERN_INFO "Device File Opened...!!!\n");
+        return 0;
+}
+
+/*
+** This fuction will be called when we close the Device file
+*/   
+static int etx_release(struct inode *inode, struct file *file)
+{
+        printk(KERN_INFO "Device File Closed...!!!\n");
+        return 0;
+}
+
+/*
+** This fuction will be called when we read the Device file
+*/
+static ssize_t etx_read(struct file *filp, 
+                char __user *buf, size_t len, loff_t *off)
+{
+        printk(KERN_INFO "Read function\n");
+ 
+        return 0;
+}
+
+/*
+** This fuction will be called when we write the Device file
+*/
+static ssize_t etx_write(struct file *filp, 
+                const char __user *buf, size_t len, loff_t *off)
+{
+        printk(KERN_INFO "Write Function\n");
+        return len;
+}
+
+/*
+** Module Init function
+*/  
+static int __init etx_driver_init(void)
+{
+        /*Allocating Major number*/
+        if((alloc_chrdev_region(&dev, 0, 1, "etx_Dev")) <0){
+                printk(KERN_INFO "Cannot allocate major number\n");
+                return -1;
+        }
+        printk(KERN_INFO "Major = %d Minor = %d \n",MAJOR(dev), MINOR(dev));
+ 
+        /*Creating cdev structure*/
+        cdev_init(&etx_cdev,&fops);
+ 
+        /*Adding character device to the system*/
+        if((cdev_add(&etx_cdev,dev,1)) < 0){
+            printk(KERN_INFO "Cannot add the device to the system\n");
+            goto r_class;
+        }
+ 
+        /*Creating struct class*/
+        if((dev_class = class_create(THIS_MODULE,"etx_class")) == NULL){
+            printk(KERN_INFO "Cannot create the struct class\n");
+            goto r_class;
+        }
+ 
+        /*Creating device*/
+        if((device_create(dev_class,NULL,dev,NULL,"etx_device")) == NULL){
+            printk(KERN_INFO "Cannot create the Device \n");
+            goto r_device;
+        }
+ 
+        etx_thread = kthread_create(thread_function,NULL,"eTx Thread");
+        if(etx_thread) {
+            wake_up_process(etx_thread);
+        } else {
+            printk(KERN_ERR "Cannot create kthread\n");
+            goto r_device;
+        }
+#if 0
+        /* You can use this method to create and run the thread */
+        etx_thread = kthread_run(thread_function,NULL,"eTx Thread");
+        if(etx_thread) {
+            printk(KERN_ERR "Kthread Created Successfully...\n");
+        } else {
+            printk(KERN_ERR "Cannot create kthread\n");
+             goto r_device;
+        }
+#endif
+        printk(KERN_INFO "Device Driver Insert...Done!!!\n");
+        return 0;
+ 
+ 
+r_device:
+        class_destroy(dev_class);
+r_class:
+        unregister_chrdev_region(dev,1);
+        cdev_del(&etx_cdev);
+        return -1;
+}
+
+/*
+** Module exit function
+*/  
+static void __exit etx_driver_exit(void)
+{
+        kthread_stop(etx_thread);
+        device_destroy(dev_class,dev);
+        class_destroy(dev_class);
+        cdev_del(&etx_cdev);
+        unregister_chrdev_region(dev, 1);
+        printk(KERN_INFO "Device Driver Remove...Done!!\n");
+}
+ 
+module_init(etx_driver_init);
+module_exit(etx_driver_exit);
+ 
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("EmbeTronicX <embetronicx@gmail.com>");
+MODULE_DESCRIPTION("A simple device driver - Kernel Thread");
+MODULE_VERSION("1.14");
