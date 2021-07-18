80e4b8bea13c5a1061fd1f0fe42ff0c73b3a37ee
EmbeTronicX <embetronicx@gmail.com>
2021-07-18 20:33:54 +0530

    minor changes

--- Microcontrollers/STM32/STM32F7xx/Bootloader_Example/Application/Core/Src/system_stm32f7xx.c
+++ Microcontrollers/STM32/STM32F7xx/Bootloader_Example/Application/Core/Src/system_stm32f7xx.c
@@ -78,7 +78,7 @@
 /*!< Uncomment the following line if you need to relocate the vector table
      anywhere in Flash or Sram, else the vector table is kept at the automatic
      remap of boot address selected */
- #define USER_VECT_TAB_ADDRESS
+#define USER_VECT_TAB_ADDRESS
 
 #if defined(USER_VECT_TAB_ADDRESS)
 /*!< Uncomment the following line if you need to relocate your vector Table
